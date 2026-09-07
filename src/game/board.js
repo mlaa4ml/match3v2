d001e629c5a56291b6bb9f05197a12e3d1eea415
mlaa4ml <mlaa4ml@gmail.com>
2026-09-07 20:03:41 +0300

    src/game/board.js: patch

--- src/game/board.js
+++ src/game/board.js
@@ -471,7 +471,7 @@ export function resolveWave(
         row: spawnCell.row,
         col: spawnCell.col,
         special,
-        color: grid[spawnCell.row][spawnCell.col].color,
+        color: spawnColor(spawnCell.row, spawnCell.col),
       });
       spawnKeys.add(`${spawnCell.row},${spawnCell.col}`);
     }
